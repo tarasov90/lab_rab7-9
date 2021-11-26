2a8530337685ee7d8818f13651c98f4f5afef8f8
tarasov90 <kirill.tarasov081@gmail.com>
2021-11-26 21:56:35 +0300

    rab1

---
+++ lab1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace lab1
+{
+    class Program
+    {
+        class A { }
+
+        class B : A {
+            public A a;
+
+            public B(A a) {
+                this.a = a;
+            }
+        }
+
+        class C : B {
+            public A a2;
+            public B b;
+
+            public C(A a, A a2, B b) : base(a) {
+                this.a2 = a2;
+                this.b = b;
+            }
+        }
+
+        static void Main()
+        {
+            C c = new C(new A(), new A(), new B(new A()));
+        }
+    }
+}
